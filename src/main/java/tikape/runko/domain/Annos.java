aa630a1566d95769bb84e40eddaf46e01c9d3699
aie8 <ttt>
2018-06-16 11:25:55 +0300

    Raaka-aineen käsittely sisällytetty annos-luokkaan. erillinen AnnosRaakaAine-luokka on turha.

--- src/main/java/tikape/runko/domain/Annos.java
+++ src/main/java/tikape/runko/domain/Annos.java
@@ -5,19 +5,42 @@
  */
 package tikape.runko.domain;
 
+import java.util.ArrayList;
 /**
  *
  * @author antti
  */
 public class Annos {
     private Integer id;
-    private String nimi;
-
+    private String nimi; 
+    private ArrayList<reseptiRaakaAine> RaakaAineLista;
     public Annos(Integer id, String nimi) {
         this.id = id;
         this.nimi = nimi;
     }
+private class reseptiRaakaAine {
+    private RaakaAine RaakaAine;
+    private int jarjestys;
+    private int maara;
+    private String ohje;
 
+    public reseptiRaakaAine(RaakaAine RaakaAine, int jarjestys,
+            int maara, String ohje) {
+        this.RaakaAine = RaakaAine;
+        this.jarjestys=jarjestys;
+        this.maara=maara;
+        this.ohje=ohje;
+    }
+}
+
+    public reseptiRaakaAine addRaakaAine(RaakaAine raakaAine, int jarjestys,int maara, String ohje){
+        reseptiRaakaAine RA=new reseptiRaakaAine(raakaAine,jarjestys,
+            maara, ohje);
+        RaakaAineLista.add(RA);
+        return RA;
+    }
+    
+    
     public Integer getId() {
         return id;
     }
@@ -29,6 +52,7 @@ public class Annos {
     public String getNimi() {
         return nimi;
     }
+    
 
     public void setNimi(String nimi) {
         this.nimi = nimi;
