7e773ee0e5f7e991c3072ccab53207db9e3aec17
aie8 <ttt>
2018-06-17 19:28:59 +0300

    Herokun postgre lisätty

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -12,10 +12,17 @@ public class Database {
         this.databaseAddress = databaseAddress;
     }
 
-    public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(databaseAddress);
+//    public Connection getConnection() throws SQLException {
+//        return DriverManager.getConnection(databaseAddress);
+ //   }
+public static Connection getConnection() throws Exception {
+    String dbUrl = System.getenv("JDBC_DATABASE_URL");
+    if (dbUrl != null && dbUrl.length() > 0) {
+        return DriverManager.getConnection(dbUrl);
     }
 
+    return DriverManager.getConnection("jdbc:sqlite:opiskelijat.db");
+}
     public void init() {
         List<String> lauseet = sqliteLauseet();
 
