409442f16eba6537376e0ca8154cb4e975005e63
aie8 <ttt>
2018-06-17 18:58:43 +0300

    First commit to Heroku

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -15,6 +15,10 @@ import tikape.runko.domain.RaakaAine;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        if (System.getenv("PORT") != null) {
+    Spark.port(Integer.valueOf(System.getenv("PORT")));
+}
+        
         Database database = new Database("jdbc:sqlite:opiskelijat.db");
         database.init();
 
